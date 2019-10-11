f8b915a2ae351c934c9ed895d32865a3e257ec11
Andrew Duong <duongandrews@gmail.com>
2019-10-11 17:15:32 -0400

    Created helper functions

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,56 +2,20 @@ import React, { useState, useEffect } from "react";
 import DayList from "components/DayList";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0
-  }
-];
-
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  }
-];
-const [likes, setLikes] = useState(0);
-return <button onClick={() => setLikes(prev => prev + 1)} />
-
-useEffect(() => {
-  if (likes > 0) {
-    setTimeout(() => setLikes(prev => prev - 1), 1000);
-  }
-}, [likes]);
-
-
+import axios from "axios";
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+  const setDay = day => {
+    if (day !== state.day) {
+      setState(prev => ({ ...prev, day }));
+    }
+  };
   return (
     <main className="layout">
       <section className="sidebar">
@@ -81,3 +45,56 @@ export default function Application(props) {
     </main>
   );
 }
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  }
+];
+const [likes, setLikes] = useState(0);
+return <button onClick={() => setLikes(prev => prev + 1)} />
+
+useEffect(() => {
+  if (likes > 0) {
+    setTimeout(() => setLikes(prev => prev - 1), 1000);
+  }
+}, [likes]);
+
+axios
+  .get("/api/appointments")
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error.response.status);
+    console.log(error.response.headers);
+    console.log(error.response.data);
+  });
+
+axios
+  .put(`/api/appointments/2`, {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Archie Cohen",
+      interviewer: 9,
+    },
+  })
+  .then((response) => {
+    console.log(response);
+  });
+
+
